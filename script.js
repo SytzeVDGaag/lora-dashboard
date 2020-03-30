9df6c35684437ef42d10668e1c83817c32f8dcad
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2020-03-30 14:56:58 +0200

    updates API data krijgen

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-function getOption(){
+function getOption() {
     var string = document.getElementById('sort').value;
     getAPIData(string);
 }
 
 function getAPIData(sortSetting) {
-    var request = new XMLHttpRequest();
-    var url = 'https://lora2d.herokuapp.com/getnodes?filter=' + sortSetting;
-
+    /**var request = new XMLHttpRequest();
+    
     request.open('GET', url, true);
-
+    request.send();
     request.onload = function () {
-
-        var data = JSON.parse(this.response)
+        
         for (var i = 0; i < data.length; i++) {
             console.log(data[i].temperature + ' is a ' + data[i].humidity + '.');
         }
 
-    }
+    }**/
+    
+    var Http = new XMLHttpRequest();
+        var url = 'https://lora2d.herokuapp.com/getnodes?filter=' + sortSetting;
+        Http.open("GET",url);
+        Http.send();
 
-    request.send();
+        Http.onreadystatechange = (e) => 
+        {
+            console.log(Http.responseText);
+            var data = JSON.stringify(Http.responseText)
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i].temperature + ' is a ' + data[i].humidity + '.');
+            }
+        };
 }
+
