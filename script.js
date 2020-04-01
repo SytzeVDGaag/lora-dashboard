19d5282cced401796793dded06e0bb35e659a70a
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2020-04-01 15:11:20 +0200

    infoblokken vanuit de JS gemaakt en HTML file functie naam aangepast

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function getOption() {
+function getData() {
+    document.getElementById("classroomcontainer").innerHTML="";
     var string = document.getElementById('sort').value;
     getAPIData(string);
 }
@@ -15,19 +16,30 @@ function getAPIData(sortSetting) {
         }
 
     }**/
-    
-    var Http = new XMLHttpRequest();
-        var url = 'https://lora2d.herokuapp.com/getnodes?filter=' + sortSetting;
-        Http.open("GET",url);
-        Http.send();
 
-        Http.onreadystatechange = (e) => 
-        {
+    var Http = new XMLHttpRequest();
+    var url = 'https://lora2d.herokuapp.com/getnodes?filter=' + sortSetting;
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        if (Http.readyState == 4 && Http.status == 200) {
+            var data = JSON.parse(Http.responseText);
             console.log(Http.responseText);
-            var data = JSON.stringify(Http.responseText)
-            for (var i = 0; i < data.length; i++) {
-                console.log(data[i].temperature + ' is a ' + data[i].humidity + '.');
+            for (var i = 0; i < data.nodes.length; i++) {
+                createElement(data.nodes[i]);
             }
-        };
+        }
+    }
 }
+
+function createElement(data){
+    var roomcontainer = document.createElement("a",);
+    roomcontainer.classList.add("roomstats");
+    roomcontainer.setAttribute("href", "classroomINT.html")
+    roomcontainer.innerHTML = "<div class='classroom'><h2>1.016</h2></div><div class='stat'><img src='img/tempIMG.png' alt='thermometer'><h3>" + data.temperature + "&deg;C</h3></div><div class='stat'><img src='img/humidIMG.png' alt='waterdrop'><h3>" + data.humidity + "%</h3></div>";
+    var block = document.getElementById("classroomcontainer");
+    block.appendChild(roomcontainer);
+}
+
 
