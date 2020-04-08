e8b4a8d3165ae70212f3c36d09e37323e68b0245
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2020-04-08 11:16:53 +0200

    classroominfo creation

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ function getData() {
 }
 
 function getAPIData(sortSetting) {
-    /**var request = new XMLHttpRequest();
-    
-    request.open('GET', url, true);
-    request.send();
-    request.onload = function () {
-        
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].temperature + ' is a ' + data[i].humidity + '.');
-        }
-
-    }**/
-
     var Http = new XMLHttpRequest();
     var url = 'https://lora2d.herokuapp.com/getnodes?filter=' + sortSetting;
     Http.open("GET", url);
@@ -25,7 +13,6 @@ function getAPIData(sortSetting) {
     Http.onreadystatechange = (e) => {
         if (Http.readyState == 4 && Http.status == 200) {
             var data = JSON.parse(Http.responseText);
-            console.log(Http.responseText);
             for (var i = 0; i < data.nodes.length; i++) {
                 createElement(data.nodes[i]);
             }
@@ -36,10 +23,29 @@ function getAPIData(sortSetting) {
 function createElement(data){
     var roomcontainer = document.createElement("a",);
     roomcontainer.classList.add("roomstats");
-    roomcontainer.setAttribute("href", "classroomINT.html")
+    roomcontainer.setAttribute("href", "classroominfo.html?GET=" + data.nodeID)
     roomcontainer.innerHTML = "<div class='classroom'><h2>1.016</h2></div><div class='stat'><img src='img/tempIMG.png' alt='thermometer'><h3>" + data.temperature + "&deg;C</h3></div><div class='stat'><img src='img/humidIMG.png' alt='waterdrop'><h3>" + data.humidity + "%</h3></div>";
     var block = document.getElementById("classroomcontainer");
     block.appendChild(roomcontainer);
 }
 
+function getAPIInfo() {
+    var Http = new XMLHttpRequest();
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const nodeID = urlParams.get('GET');
+    var url = 'https://lora2d.herokuapp.com/getalldatafornode?node=' + nodeID;
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange = (e) => {
+        if (Http.readyState == 4 && Http.status == 200) {
+            var data = JSON.parse(Http.responseText);
+            console.log(Http.responseText);
+            getInformation(data.measurements)
+        }
+    }
+}
 
+function getInformation(information){
+
+}
