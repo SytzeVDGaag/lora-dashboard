7bb08b2609c511b6d0608f2a7f52c744e76f9e38
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2020-04-09 19:44:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function createElement(data) {
     var roomcontainer = document.createElement("a");
     roomcontainer.classList.add("roomstats");
     roomcontainer.setAttribute("href", "classroominfo.html?GET=" + data.nodeID)
-    roomcontainer.innerHTML = "<div class='classroom'><h2>" + data.locationname + "</h2></div><div class='stat'><img src='img/tempIMG.png' alt='thermometer'><h3>" + data.temperature + "&deg;C</h3></div><div class='stat'><img src='img/humidIMG.png' alt='waterdrop'><h3>" + data.humidity + "%</h3></div>";
+    roomcontainer.innerHTML = "<div class='classroom'><h2>" + data.location + "</h2></div><div class='stat'><img src='img/tempIMG.png' alt='thermometer'><h3>" + data.temperature + "&deg;C</h3></div><div class='stat'><img src='img/humidIMG.png' alt='waterdrop'><h3>" + data.humidity + "%</h3></div>";
     var block = document.getElementById("classroomcontainer");
     block.appendChild(roomcontainer);
 }
@@ -34,7 +34,7 @@ function getAPIInfo() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const nodeID = urlParams.get('GET');
-    var url = 'https://lora2d.herokuapp.com/measurements?node=' + nodeID;
+    var url = 'https://lora2d.herokuapp.com/measurements/' + nodeID;
     Http.open("GET", url);
     Http.send();
     Http.onreadystatechange = (e) => {
